18a60165ac620c5f436474324f5281c2b2a62a5a
Nguyen Phi Hieu <107748419+BrianNguyen1507@users.noreply.github.com>
2024-10-17 17:35:27 +0700

    fix navbar

--- src/components/CorporateClients.tsx
+++ src/components/CorporateClients.tsx
@@ -153,8 +153,7 @@ const ClientsSection: React.FC<{
               key={index}
               className="scrolling-item flex items-center justify-center mx-2"
               data-tip={client.name}
-              initial={{ opacity: 0, y: 100 }}
-              animate={isVisible ? { opacity: 1, y: 0 } : { opacity: 0, y: 50 }}
+              animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5 }}
               onClick={() => handleItemClick(index)}
             >
